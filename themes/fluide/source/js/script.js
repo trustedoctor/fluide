4dfee12bd95f701bd2bc71537a8f8f99770ddbb3
Krystian Drożdżyński <krystian@drozdzynski.me>
2018-06-29 11:45:55 +0200

    Site updated: 2018-06-29 11:45:54

--- themes/fluide/source/js/script.js
+++ themes/fluide/source/js/script.js
@@ -2,7 +2,19 @@ if (document.querySelectorAll('.docs aside').length > 0) {
   let aside   = document.querySelector('.docs aside'),
       expand  = aside.querySelector('.expand')
 
-  expand.addEventListener('click', function(e) {
+  // expand.addEventListener('click', function(e) {
 
-  })
+  // })
 }
+
+var headerScrolled = function() {
+  var header = document.querySelector('header')
+  if (document.documentElement.scrollTop > 0) {
+    if (header.className.indexOf('scrolled') == -1) header.className += ' scrolled'
+  } else {
+    header.className = header.className.replace('scrolled', '')
+  }
+}
+
+headerScrolled()
+document.addEventListener('scroll', headerScrolled)
