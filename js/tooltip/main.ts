d96ba05f8b8105e8f10a453fe886d736ad579880
Krystian Drożdżyński <krystian@drozdzynski.me>
2018-07-18 13:22:40 +0200

    Added tooltip positions

--- js/tooltip/main.ts
+++ js/tooltip/main.ts
@@ -1,10 +1,31 @@
 import Module from '../module';
 
-export default class Tooltip extends Module {
-  private tooltip: HTMLElement
+enum Position {
+  TOP, BOTTOM, LEFT, RIGHT, CLASS,
+}
 
-  constructor(el: HTMLElement | string) {
+export default class Tooltip extends Module {
+  public static Position = Position
+
+  private tooltip: HTMLElement
+  private position: Position
+
+  constructor(el: HTMLElement | string, position: Position = Position.CLASS) {
     super(el)
+
+    if (position === Position.CLASS) {
+      if (this.el.className.indexOf('tooltip-top') > -1) {
+        this.position = Position.TOP
+      } else if (this.el.className.indexOf('tooltip-left') > -1) {
+        this.position = Position.LEFT
+      } else if (this.el.className.indexOf('tooltip-right') > -1) {
+        this.position = Position.RIGHT
+      } else {
+        this.position = Position.BOTTOM
+      }
+    } else {
+      this.position = position
+    }
 
     this.el.onmouseenter = (event) => this.mouseEnter(event)
     this.el.onmouseleave = (event) => this.mouseLeave(event)
@@ -19,7 +40,7 @@ export default class Tooltip extends Module {
 
     this.el.parentElement.insertBefore(this.tooltip, this.el.nextSibling)
 
-    const { left, top } = this.getPosition()
+    const { left, top } = this.calculatePosition()
     this.tooltip.style.left = left + 'px'
     this.tooltip.style.top = top + 'px'
   }
@@ -28,9 +49,23 @@ export default class Tooltip extends Module {
     this.el.parentElement.removeChild(this.tooltip)
   }
 
-  private getPosition() {
-    const left = this.el.offsetLeft + (this.el.clientWidth / 2) - (this.tooltip.clientWidth / 2)
-    const top = this.el.offsetTop + this.el.clientHeight + 5
+  private calculatePosition() {
+    let left
+    let top
+
+    if (this.position === Position.BOTTOM) {
+      left = this.el.offsetLeft + (this.el.clientWidth / 2) - (this.tooltip.clientWidth / 2)
+      top = this.el.offsetTop + this.el.clientHeight + 5
+    } else if (this.position === Position.TOP) {
+      left = this.el.offsetLeft + (this.el.clientWidth / 2) - (this.tooltip.clientWidth / 2)
+      top = this.el.offsetTop - this.tooltip.clientHeight - 5
+    } else if (this.position === Position.LEFT) {
+      left = this.el.offsetLeft - this.tooltip.clientWidth - 5
+      top = this.el.offsetTop + (this.el.clientHeight / 2) - (this.tooltip.clientHeight / 2)
+    } else if (this.position === Position.RIGHT) {
+      left = this.el.offsetLeft + this.el.clientWidth + 5
+      top = this.el.offsetTop + (this.el.clientHeight / 2) - (this.tooltip.clientHeight / 2)
+    }
 
     return {
       left, top,
